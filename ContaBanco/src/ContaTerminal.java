006f541e68468173761a04e99f6ef13e72531638
silviomancilha <mancilha.silvio@gmail.com>
2024-05-02 22:34:02 -0300

    acrescentado linha para fechamento do scanner

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,26 +3,28 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        // TODO: Conhecer e importar a calse Scanner
+        // Conhecer e importar a calse Scanner
 
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         // Exibir as mensagens para o usuário e obter os valores digitados no terninal
-        System.out.println("Por favor, digite o número da Agência !");
+        System.out.println("Por favor, digite o número da Agência : ");
         String numeroAgencia = scanner.next(); //utilizado String, pois agência pode começar com digito zero
        
-        System.out.println("Por favor, digite o número da conta !");
+        System.out.println("Por favor, digite o número da conta :");
         String numeroConta = scanner.next();
        
-        System.out.println("Por favor, digite o nome do cliente !");
-        String nomeCliente = scanner.next(); 
-      
-        System.out.println("Por favor, digite o saldo inicial !");
+        System.out.println("Por favor, digite o nome do cliente :\n");
+        String nomeCliente = scanner.next();
+                             
+        System.out.println("Por favor, digite o saldo inicial :");
         double valorSaldo = scanner.nextDouble();
+
+
+        scanner.close();
         
         // Exibir a mensagem da conta criada
         System.out.println("Olá" + nomeCliente + "," + "\nobrigado por criar uma conta em nosso banco,\nsua agência é " + numeroAgencia + " conta " + numeroConta + "\n e seu saldo é de R$ " + valorSaldo + " já está disponível para saque");
 
-        
     }
 }
